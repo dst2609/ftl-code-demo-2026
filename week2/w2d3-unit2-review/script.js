f0cc3640ca47085da7e451e4fbc3707edf485a1d
Devarsh Thaker <devarsht@gmail.com>
2026-06-10 10:17:41 -0700

    updated the response for better info

--- week2/w2d3-unit2-review/script.js
+++ week2/w2d3-unit2-review/script.js
@@ -2,25 +2,34 @@ const input = document.getElementById("pokemonInput");
 const button = document.getElementById("searchBtn");
 const result = document.getElementById("result");
 
-button.addEventListener("click", async () => {
+button.addEventListener("click", async function () {
   try {
-    const pokemonName = input.value.toLocaleLowerCase();
+    const pokemonName = input.value.toLowerCase();
 
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
     );
-    // if the api could not find the pokemon
+
+    // If the API couldn't find the Pokémon
     if (!response.ok) {
-      throw new Error("Pokemon not found, nooob");
+      throw new Error("Pokemon not found");
     }
 
     const data = await response.json();
 
     result.innerHTML = `
-    <h2>${data.name}</h2>
-    <img src="${data.sprites.front_default}" />
+      <h2>${data.name}</h2>
+      <img src="${data.sprites.front_default}" />
+      <p>Height: ${data.height}</p>
+      <p>Weight: ${data.weight}</p>
+      <p>Type: ${data.types[0].type.name}</p>
     `;
   } catch (error) {
-    console.error(error);
+    result.innerHTML = `
+      <h2>Oops!</h2>
+      <p>Pokemon not found. Try again.</p>
+    `;
+
+    console.log(error);
   }
 });
