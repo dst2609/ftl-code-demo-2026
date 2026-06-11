839ef508a7e7423db5765a06df88df05bd2b630c
Devarsh Thaker <devarsht@gmail.com>
2026-06-11 12:03:14 -0700

    first react intro demo

---
+++ week2/w2d4-react-intro/pokemon-demo-ui/src/App.jsx
@@ -0,0 +1,12 @@
+import PokemonCard from "./Components/PokemonCard/PokemonCard";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <PokemonCard />
+    </div>
+  );
+};
+
+export default App;
---
+++ week2/w2d4-react-intro/pokemon-demo-ui/src/Components/PokemonCard/PokemonCard.jsx
@@ -0,0 +1,14 @@
+const PokemonCard = () => {
+  //this is where you JS goes
+  return (
+    <div>
+      <h2>Pikachu</h2>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+        alt="Pikachu"
+      />
+    </div>
+  );
+};
+
+export default PokemonCard;
