08a9cf8babb76288183b02ddf2e92a8f35012074
Devarsh Thaker <devarsht@gmail.com>
2026-06-29 15:06:29 -0700

    updated login ui component

--- week5/user-auth-ui/src/Components/Login.jsx
+++ week5/user-auth-ui/src/Components/Login.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const API = import.meta.env.VITE_API_URL;
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(`${API}/users/login`, {
+        email,
+        password,
+      });
+      //store token in localStorage
+      localStorage.setItem("token", response.data.token);
+      navigate("/dashboard");
+    } catch (error) {
+      alert("Login failed, check credentials"); //for demo, IRL change this to dyncamically show error
+    }
+  };
+
+  return (
+    <div className="container">
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={() => navigate("/register")}>Go to Register</button>
+    </div>
+  );
 };
 
 export default Login;
